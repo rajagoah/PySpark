51f1602f887899564d657106e75bc389395451ae
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-03-11 09:24:09 -0400

    updated code

--- RDD.py
+++ RDD.py
@@ -1,17 +1,18 @@
 import pyspark as ps
-
-sc = ps.SparkContext("local", "RDD")
-string = "hell"
-
-str_rdd = sc.parallelize(string)
-
-#parallelizing the string and counting the number of variables in the list
-print(str_rdd.count())
-
-#returning the elements in an RDD
-print(str_rdd.collect())
-
-#printing all the elements of an RDD
-def f(x):
+def print_function(x):
     print(x)
-    print(str_rdd.foreach(f))
+
+if __name__ == "__main__":
+    sc = ps.SparkContext("local", "RDD")
+    string = "hell0"
+
+    str_rdd = sc.parallelize(string)
+
+    #parallelizing the string and counting the number of variables in the list
+    print(str_rdd.count())
+
+    #returning the elements in an RDD
+    print(str_rdd.collect())
+
+    #printing all the elements of an RDD. the foreach function is an action. this action opeartion essentially passes each element of the rdd to the print_function() method
+    str_rdd.foreach(print_function)
